463fd76c7ee52f7fb519bb7127a7ce8f79a54229
Neerajkumarc <chaudharyneeraj91@gmail.com>
2023-09-10 00:24:28 -0700

    new twitter icon & package update

--- constants/socialLinks.js
+++ constants/socialLinks.js
@@ -1,7 +1,7 @@
 const socialLinks = [
   "https://github.com/Neerajkumarc",
   "https://www.linkedin.com/in/neerajkumar31/",
-  "https://twitter.com/neerajkumar_31",
+  "https://x.com/neerajkumar_31",
   "https://www.instagram.com/neerajchaudhary.31/",
   "https://www.facebook.com/neerajkumar.chaudhary.94695/",
 ];
