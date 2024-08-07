9ff50188acbf9738b9aafa3446978440ebf21811
neerajkumarc <chaudharyneeraj91@gmail.com>
2024-08-07 22:57:22 +0530

    update instagram link

--- constants/socialLinks.js
+++ constants/socialLinks.js
@@ -2,7 +2,7 @@ const socialLinks = [
   "https://github.com/Neerajkumarc",
   "https://www.linkedin.com/in/neerajkumar31/",
   "https://x.com/neerajkumar_31",
-  "https://www.instagram.com/neerajchaudhary.31/",
+  "https://www.instagram.com/neeraj0x1f/",
   "https://www.facebook.com/neerajkumar.chaudhary.94695/",
 ];
 
